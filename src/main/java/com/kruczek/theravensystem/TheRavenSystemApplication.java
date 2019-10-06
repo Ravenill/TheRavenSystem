a2a9a78c058b5d6cb89d0ef5571e05718225c6d1
Ravenill <tarczewski.k@outlook.com>
2019-10-06 10:12:18 +0200

    Spring boot initial commit

---
+++ src/main/java/com/kruczek/theravensystem/TheRavenSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kruczek.theravensystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRavenSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRavenSystemApplication.class, args);
+	}
+
+}
